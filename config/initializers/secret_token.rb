05bc226c098e3983e570730c61fbe2487bab2abd
Brian McClain <brianmmcclain@gmail.com>
2012-03-05 00:22:39 -0500

    Standard Rails boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlogTemplate::Application.config.secret_token = 'b12db0105efc5e697aad4f2088af57eedaf9f72a9dd2ba77ae4a63aea9621c0f79efd696b5bd1f1608a567fd15361ab0463eaedc15c97826b373f59e6326f4f3'
